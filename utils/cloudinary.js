886ed897162799a758f99ca0be1a099970d2422b
Ankur kumar <ankurkumar89632@gmail.com>
2025-09-20 19:54:49 +0530

    initial commit

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,18 +1,18 @@
-const cloudinary = require("cloudinary").v2;
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
+// const cloudinary = require("cloudinary").v2;
+// const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
-});
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_KEY,
+//   api_secret: process.env.CLOUDINARY_SECRET,
+// });
 
-const storage = new CloudinaryStorage({
-  cloudinary,
-  params: {
-    folder: "restaurant_images",
-    allowed_formats: ["jpg", "png", "jpeg"],
-  },
-});
+// const storage = new CloudinaryStorage({
+//   cloudinary,
+//   params: {
+//     folder: "restaurant_images",
+//     allowed_formats: ["jpg", "png", "jpeg"],
+//   },
+// });
 
-module.exports = { cloudinary, storage };
+// module.exports = { cloudinary, storage };
--- utils/multer.js
+++ utils/multer.js
@@ -1,7 +1,7 @@
-utils/multer.js
-const multer = require("multer");
-const { storage } = require("./cloudinary"); // relative to utils/
+// utils/multer.js
+// const multer = require("multer");
+// const { storage } = require("./cloudinary"); // relative to utils/
 
-const upload = multer({ storage });
+// const upload = multer({ storage });
 
-module.exports = upload;
+// module.exports = upload;
