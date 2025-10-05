215ecbc8c3ea518293dc187e7de63530cc767eb4
Ankur kumar <ankurkumar89632@gmail.com>
2025-10-05 18:40:58 +0530

    initial commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 // routes/authRoutes.js
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("../models/user");
 const nodemailer = require("nodemailer");
 
 // Render forgot page
